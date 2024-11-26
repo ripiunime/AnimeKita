ce6353c1fb60850ea99a07c426a94c51f2b71d1d
Haekalsss <kallsss145@gmail.com>
2024-11-26 14:47:55 +0700

    navbar completed import

--- app/components/navbar/index.js
+++ app/components/navbar/index.js
@@ -1,9 +1,12 @@
+'use client';
+
 // /components/navbar/index.js
 export default function Navbar() {
     console.log('Navbar component loaded');  // This will show up in the console if it's rendered
     return (
       <nav>
         <ul>
+          <h1>AnimeKita</h1>
           <li><a href="/dashboard/user">Dashboard</a></li>
           <li><a href="../profile/[user-id]">Profile</a></li>
           <li><a href="/auth/user-login">Login</a></li>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import Navbar from '../../../components/navbar'; // Adjust relative import
+import Navbar from './components/navbar'; // Adjust relative import
 
 console.log('Navbar imported:', Navbar);
 
@@ -11,22 +11,6 @@ export default function Dashboard() {
     return (
         <div className="dashboard">
             <Navbar /> {/* Include Navbar here */}
-            
-            <h1 className="dashboard-title">Selamat Datang di Dashboard</h1>
-            <nav className="dashboard-nav">
-                <button
-                    onClick={() => router.push('/auth/user-login')}
-                    className="dashboard-button login-button"
-                >
-                    Login
-                </button>
-                <button
-                    onClick={() => router.push('/auth/user-register')}
-                    className="dashboard-button register-button"
-                >
-                    Register
-                </button>
-            </nav>
         </div>
     );
 }
